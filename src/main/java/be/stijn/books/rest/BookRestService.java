e38ef1f4f7ec71d3b4796396344c9026a2cc3a55
Stijn Van Bever <svanbever1@gmail.com>
2016-10-18 21:50:23 +0200

    extracted persistence to booksService

--- src/main/java/be/stijn/books/rest/BookRestService.java
+++ src/main/java/be/stijn/books/rest/BookRestService.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateful;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -14,36 +12,24 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import be.stijn.books.model.Book;
-import be.stijn.books.service.TestEJB;
+import be.stijn.books.service.BooksEJB;
 
 @Path("books")
 @Stateful
 public class BookRestService {
 	
-	@PersistenceContext(unitName = "books")
-	private EntityManager em;
-	
 	@EJB
-	private TestEJB testEJB;
+	private BooksEJB booksService;
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Book> getAllBooks() {
-		List<Book> books = em.createQuery("from Book", Book.class).getResultList();
-		
-		return books;
+		return booksService.getAllBooks();
 	}
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void addBook(Book book) {
-		em.persist(book);
-	}
-	
-	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	@Path("test")
-	public String test() {
-		return testEJB.getTest();
+		booksService.addBook(book);
 	}
 }
--- src/main/java/be/stijn/books/service/BooksEJB.java
+++ src/main/java/be/stijn/books/service/BooksEJB.java
@@ -1,10 +1,26 @@
 package be.stijn.books.service;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import be.stijn.books.model.Book;
 
 @Stateless
-public class TestEJB {
-	public String getTest() {
-		return "Test Success!";
+public class BooksEJB {
+	
+	@PersistenceContext(unitName = "books")
+	private EntityManager em;
+	
+	public List<Book> getAllBooks() {
+		List<Book> books = em.createQuery("from Book", Book.class).getResultList();
+		
+		return books;
+	}
+	
+	public void addBook(Book book) {
+		em.persist(book);
 	}
 }
